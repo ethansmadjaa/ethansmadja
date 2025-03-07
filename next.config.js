09e3ea758e5be33ee56560f16a77ee995ee106d4
ethansmadjaa <ethansmadja@icloud.com>
2025-03-07 14:46:04 +0100

    updated to move to src

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
