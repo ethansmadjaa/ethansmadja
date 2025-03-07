3069ed5f78b1d1729aadbc92094c8cda37bfde15
ethansmadjaa <ethansmadja@icloud.com>
2025-03-07 20:17:59 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
         sm: 'calc(var(--radius) - 4px)',
       },
       colors: {
+        'custom-blue': '#00AAFF',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         card: {
