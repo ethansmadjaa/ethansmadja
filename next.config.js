8badd0f54b6713b16da8234d926a8012d37fd48a
ethansmadjaa <ethansmadja@icloud.com>
2025-03-07 14:43:42 +0100

    update to get code from source

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
